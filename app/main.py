3441e121fbbb94da7231802d0e68ec0a2f4ea097
Jack <jmos0011@student.monash.edu>
2025-03-13 13:44:45 +1100

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -3,18 +3,11 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            print("data: %s" % data)
-            if data == b"*1\r\n$4\r\nping\r\n":
-                conn.sendall(b"+PONG\r\n")
-            elif b"ping" in data:
-                conn.sendall(b"+PONG\r\n")
-            else:
-                conn.sendall(b"error\r\n")
+    connection, _ = server_socket.accept()  # wait for client
+    while True:
+        data = connection.recv(1024)
+        if data:
+            connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
